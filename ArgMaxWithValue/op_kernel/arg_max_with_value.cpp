f358a78d876f8ef55bd000a28d119ec4a837432e
tangyh <71632604+tangyhang@users.noreply.github.com>
2024-12-08 21:07:48 +0800

    Update arg_max_with_value.cpp
    删除half中的tiling

--- ArgMaxWithValue/op_kernel/arg_max_with_value.cpp
+++ ArgMaxWithValue/op_kernel/arg_max_with_value.cpp
@@ -9,47 +9,38 @@ public:
     __aicore__ inline KernelArgMaxWithValuehalf() {}
 
     __aicore__ inline void Init(GM_ADDR x, GM_ADDR indice, GM_ADDR values, uint32_t reduceAxisLen, uint32_t subTensorLen, uint32_t outputLength,
-                                uint32_t bigCoreNum, uint32_t bigCoreProcessNum, uint32_t smallCoreNum, uint32_t smallCoreProcessNum, uint32_t singleProcessNum, uint32_t bigTileNum, uint32_t bigTailNum, uint32_t smallTileNum, uint32_t smallTailNum)
+                                uint32_t bigCoreNum, uint32_t bigCoreProcessNum, uint32_t smallCoreNum, uint32_t smallCoreProcessNum)
     {
         uint32_t coreNum = GetBlockIdx();
         uint32_t globalBufferIndex = bigCoreProcessNum * GetBlockIdx();
         this->reduceAxisLen = reduceAxisLen;
         this->subTensorLen = subTensorLen;
         this->outputLength = outputLength;
-        this->singleProcessNum = singleProcessNum;
         if (coreNum < bigCoreNum)
         {
             this->processNum = bigCoreProcessNum;
-            this->tileNum = bigTileNum;
-            this->tailProcessNum = bigTailNum;
         }
         else
         {
             this->processNum = smallCoreProcessNum;
-            this->tileNum = smallTileNum;
-            this->tailProcessNum = smallTailNum;
             globalBufferIndex -= (bigCoreProcessNum - smallCoreProcessNum) * (GetBlockIdx() - bigCoreNum);
         }
         xGm.SetGlobalBuffer((__gm__ half *)x + globalBufferIndex * reduceAxisLen * subTensorLen, reduceAxisLen * subTensorLen * processNum);
         yGm.SetGlobalBuffer((__gm__ int32_t *)indice + globalBufferIndex * subTensorLen, subTensorLen * processNum);
         zGm.SetGlobalBuffer((__gm__ half *)values + globalBufferIndex * subTensorLen, subTensorLen * processNum);
-        pipe.InitBuffer(inQueueX, BUFFER_NUM, this->singleProcessNum * ((reduceAxisLen * subTensorLen * sizeof(half) + 31) / 32 * 32));
-        pipe.InitBuffer(outQueueY, BUFFER_NUM, this->singleProcessNum * ((subTensorLen * sizeof(int32_t) + 31) / 32 * 32));
-        pipe.InitBuffer(outQueueZ, BUFFER_NUM, this->singleProcessNum * ((subTensorLen * sizeof(half) + 31) / 32 * 32));
-        pipe.InitBuffer(workQueue1, BUFFER_NUM, this->singleProcessNum * ((reduceAxisLen * sizeof(uint32_t) + 31) / 32 * 32));
-        pipe.InitBuffer(workQueue2, BUFFER_NUM, this->singleProcessNum * ((reduceAxisLen * sizeof(half) + 31) / 32 * 32));
-        pipe.InitBuffer(workQueue3, BUFFER_NUM, this->singleProcessNum * ((reduceAxisLen * sizeof(half) + 31) / 32 * 32));
-        pipe.InitBuffer(workQueue4, BUFFER_NUM, this->singleProcessNum * ((reduceAxisLen * sizeof(half) + 31) / 32 * 32));
+        pipe.InitBuffer(inQueueX, BUFFER_NUM, (reduceAxisLen * subTensorLen * sizeof(half) + 31) / 32 * 32);
+        pipe.InitBuffer(outQueueY, BUFFER_NUM, (subTensorLen * sizeof(int32_t) + 31) / 32 * 32);
+        pipe.InitBuffer(outQueueZ, BUFFER_NUM, (subTensorLen * sizeof(half) + 31) / 32 * 32);
+        pipe.InitBuffer(workQueue1, BUFFER_NUM, (reduceAxisLen * sizeof(uint32_t) + 31) / 32 * 32);
+        pipe.InitBuffer(workQueue2, BUFFER_NUM, (reduceAxisLen * sizeof(half) + 31) / 32 * 32);
+        pipe.InitBuffer(workQueue3, BUFFER_NUM, (reduceAxisLen * sizeof(half) + 31) / 32 * 32);
+        pipe.InitBuffer(workQueue4, BUFFER_NUM, (reduceAxisLen * sizeof(half) + 31) / 32 * 32);
     }
     __aicore__ inline void Process()
     {
-        uint32_t loopCount = this->tileNum;
-        this->curProcessNum = this->singleProcessNum;
+        uint32_t loopCount = this->processNum;
         for (uint32_t i = 0; i < loopCount; i++)
         {
-            if(i == this->tileNum - 1){
-                this->curProcessNum = this->tailProcessNum;
-            }
             CopyIn(i);
             Compute(i);
             CopyOut(i);
@@ -61,9 +52,9 @@ private:
     {
         LocalTensor<half> x_local = inQueueX.AllocTensor<half>();
         uint32_t copysz = this->reduceAxisLen * this->subTensorLen * sizeof(half);
-        DataCopyExtParams copyParams{static_cast<uint16_t>(this->curProcessNum), copysz, 0, 0, 0};
+        DataCopyExtParams copyParams{1, copysz, 0, 0, 0};
         DataCopyPadExtParams<half> padParams{true, 0, 0, 0};
-        DataCopyPad(x_local, xGm[progress * this->reduceAxisLen * this->subTensorLen * this->curProcessNum], copyParams, padParams);
+        DataCopyPad(x_local, xGm[progress * this->reduceAxisLen * this->subTensorLen], copyParams, padParams);
         // DumpTensor(x_local,5, copysz);
         inQueueX.EnQue(x_local);
     }
@@ -76,32 +67,21 @@ private:
         LocalTensor<half> src_local = workQueue2.AllocTensor<half>();
         LocalTensor<half> dst_local = workQueue3.AllocTensor<half>();
         LocalTensor<half> work_local = workQueue4.AllocTensor<half>();
-        for (size_t j = 0; j < this->curProcessNum; j ++)
+        for (size_t i = 0; i < reduceAxisLen; i++)
         {
-            // x_local += j * (reduceAxisLen * subTensorLen * sizeof(half) + 31) / 32 * 32 / sizeof(half);
-            // y_local += j * (subTensorLen * sizeof(int32_t) + 31) / 32 * 32 / sizeof(int32_t);
-            // z_local += j * (subTensorLen * sizeof(half) + 31) / 32 * 32 / sizeof(half);
-            // off_local += j * (reduceAxisLen * sizeof(uint32_t) + 31) / 32 * 32 / sizeof(uint32_t)sizeof(uint32_t);
-            // src_local += j * (reduceAxisLen * sizeof(half) + 31) / 32 * 32 / sizeof(half);
-            // dst_local += j * (reduceAxisLen * sizeof(half) + 31) / 32 * 32 / sizeof(half);
-            // work_local += j * (reduceAxisLen * sizeof(half) + 31) / 32 * 32 / sizeof(half);
-            for (size_t i = 0; i < reduceAxisLen; i++)
-            {
-                off_local.SetValue(i, i * subTensorLen * sizeof(half));
-            }
-            for (size_t i = 0; i < subTensorLen; i++)
-            {
-                Gather(src_local, x_local[j * ((reduceAxisLen * subTensorLen * sizeof(half) + 31) / 32 * 32 / sizeof(half))], off_local, (uint32_t)(i * sizeof(half)), reduceAxisLen);
-                PipeBarrier<PIPE_V>();
-                ReduceMax<half>(dst_local, src_local, work_local, reduceAxisLen, true);
-                half mx = dst_local.GetValue(0);
-                half index_half = dst_local.GetValue(1);
-                int32_t mx_index = *reinterpret_cast<int32_t *>(&index_half);
-                y_local.SetValue(i + j * subTensorLen, (int32_t)mx_index);
-                z_local.SetValue(i + j * subTensorLen, mx);
-            }
+            off_local.SetValue(i, i * subTensorLen * sizeof(half));
         }
-        
+        for (size_t i = 0; i < subTensorLen; i++)
+        {
+            Gather(src_local, x_local, off_local, (uint32_t)(i * sizeof(half)), reduceAxisLen);
+            PipeBarrier<PIPE_V>();
+            ReduceMax<half>(dst_local, src_local, work_local, reduceAxisLen, true);
+            half mx = dst_local.GetValue(0);
+            half index_half = dst_local.GetValue(1);
+            int32_t mx_index = *reinterpret_cast<int32_t *>(&index_half);
+            y_local.SetValue(i, (int32_t)mx_index);
+            z_local.SetValue(i, mx);
+        }
         outQueueY.EnQue(y_local);
         outQueueZ.EnQue(z_local);
         inQueueX.FreeTensor(x_local);
@@ -114,13 +94,13 @@ private:
     {
         LocalTensor<int32_t> y_local = outQueueY.DeQue<int32_t>();
         LocalTensor<half> z_local = outQueueZ.DeQue<half>();
-        uint32_t copysz1 = this->subTensorLen * sizeof(int32_t) * this->curProcessNum;
-        uint32_t copysz2 = this->subTensorLen * sizeof(half) * this->curProcessNum;
+        uint32_t copysz1 = this->subTensorLen * sizeof(int32_t);
+        uint32_t copysz2 = this->subTensorLen * sizeof(half);
         DataCopyExtParams copyParams1{1, copysz1, 0, 0, 0};
         DataCopyExtParams copyParams2{1, copysz2, 0, 0, 0};
         DataCopyPadExtParams<half> padParams1{true, 0, 0, 0};
-        DataCopyPad(yGm[progress * this->subTensorLen * this->curProcessNum], y_local, copyParams1);
-        DataCopyPad(zGm[progress * this->subTensorLen * this->curProcessNum], z_local, copyParams2);
+        DataCopyPad(yGm[progress * this->subTensorLen], y_local, copyParams1);
+        DataCopyPad(zGm[progress * this->subTensorLen], z_local, copyParams2);
         outQueueY.FreeTensor(y_local);
         outQueueZ.FreeTensor(z_local);
     }
@@ -141,10 +121,6 @@ private:
     uint32_t subTensorLen;
     uint32_t outputLength;
     uint32_t processNum;
-    uint32_t tileNum;
-    uint32_t singleProcessNum;
-    uint32_t tailProcessNum;
-    uint32_t curProcessNum;
 };
 
 class KernelArgMaxWithValuefloat
@@ -538,7 +514,7 @@ extern "C" __global__ __aicore__ void arg_max_with_value(GM_ADDR x, GM_ADDR indi
     else if(TILING_KEY_IS(3)) {
         KernelArgMaxWithValuehalf op;
         op.Init(x, indice, values, tiling_data.reduceAxisLen, tiling_data.subTensorLen, tiling_data.outputLength, 
-            tiling_data.bigCoreNum, tiling_data.bigCoreProcessNum, tiling_data.smallCoreNum, tiling_data.smallCoreProcessNum, tiling_data.singleProcessNum, tiling_data.bigTileNum, tiling_data.bigTailNum, tiling_data.smallTileNum, tiling_data.smallTailNum);
+            tiling_data.bigCoreNum, tiling_data.bigCoreProcessNum, tiling_data.smallCoreNum, tiling_data.smallCoreProcessNum);
         op.Process();
     }
     else if(TILING_KEY_IS(4)) {
